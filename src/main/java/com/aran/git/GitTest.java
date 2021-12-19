eabcb5c39bc1d6a9fa169c9aa7777f6768874ec4
aran <1440308013@qq.com>
2021-12-19 18:19:42 +0800

    First Commit

---
+++ src/main/java/com/aran/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.aran.git;
+
+/**
+ * @author ARan
+ * @create 2021-12-19 18:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
